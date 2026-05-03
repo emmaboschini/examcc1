ca8137b2aa03ac4e089aefa5be51a7819774011a
emmaboschini <emma.boschini@gmail.com>
2026-05-03 12:45:40 +0000

    Add interactive population map of South Sudan (Under 17)

---
+++ csv_to_js.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+# Convert CSV to a JS variable
+data = {}
+with open('population_under_17.csv', mode='r', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        data[row['Pcode']] = {
+            'State': row['State'],
+            'County': row['County'],
+            'Population': int(row['Population_Under_17'])
+        }
+
+with open('population_data.js', 'w') as f:
+    f.write('const populationData = ' + json.dumps(data, indent=2) + ';')
+
+print("Created population_data.js")
---
+++ population_data.js
@@ -0,0 +1,397 @@
+const populationData = {
+  "SS0001": {
+    "State": "Abyei Administrative Area",
+    "County": "Abyei Administrative Area",
+    "Population": 77766
+  },
+  "SS0101": {
+    "State": "Central Equatoria",
+    "County": "Juba",
+    "Population": 276512
+  },
+  "SS0102": {
+    "State": "Central Equatoria",
+    "County": "Kajo-keji",
+    "Population": 41793
+  },
+  "SS0103": {
+    "State": "Central Equatoria",
+    "County": "Lainya",
+    "Population": 50045
+  },
+  "SS0104": {
+    "State": "Central Equatoria",
+    "County": "Morobo",
+    "Population": 72108
+  },
+  "SS0105": {
+    "State": "Central Equatoria",
+    "County": "Terekeka",
+    "Population": 137773
+  },
+  "SS0106": {
+    "State": "Central Equatoria",
+    "County": "Yei",
+    "Population": 180443
+  },
+  "SS0201": {
+    "State": "Eastern Equatoria",
+    "County": "Budi",
+    "Population": 61115
+  },
+  "SS0202": {
+    "State": "Eastern Equatoria",
+    "County": "Ikotos",
+    "Population": 59475
+  },
+  "SS0203": {
+    "State": "Eastern Equatoria",
+    "County": "Kapoeta East",
+    "Population": 98165
+  },
+  "SS0204": {
+    "State": "Eastern Equatoria",
+    "County": "Kapoeta North",
+    "Population": 96484
+  },
+  "SS0205": {
+    "State": "Eastern Equatoria",
+    "County": "Kapoeta South",
+    "Population": 65291
+  },
+  "SS0206": {
+    "State": "Eastern Equatoria",
+    "County": "Lafon",
+    "Population": 95968
+  },
+  "SS0207": {
+    "State": "Eastern Equatoria",
+    "County": "Magwi",
+    "Population": 167577
+  },
+  "SS0208": {
+    "State": "Eastern Equatoria",
+    "County": "Torit",
+    "Population": 36543
+  },
+  "SS0301": {
+    "State": "Jonglei",
+    "County": "Akobo",
+    "Population": 120521
+  },
+  "SS0302": {
+    "State": "Jonglei",
+    "County": "Ayod",
+    "Population": 103615
+  },
+  "SS0303": {
+    "State": "Jonglei",
+    "County": "Bor South",
+    "Population": 193211
+  },
+  "SS0304": {
+    "State": "Jonglei",
+    "County": "Canal/Pigi",
+    "Population": 50213
+  },
+  "SS0305": {
+    "State": "Jonglei",
+    "County": "Duk",
+    "Population": 112113
+  },
+  "SS0306": {
+    "State": "Jonglei",
+    "County": "Fangak",
+    "Population": 124388
+  },
+  "SS0307": {
+    "State": "Jonglei",
+    "County": "Nyirol",
+    "Population": 88283
+  },
+  "SS0308": {
+    "State": "Jonglei",
+    "County": "Pibor",
+    "Population": 129995
+  },
+  "SS0309": {
+    "State": "Jonglei",
+    "County": "Pochalla",
+    "Population": 50218
+  },
+  "SS0310": {
+    "State": "Jonglei",
+    "County": "Twic East",
+    "Population": 62587
+  },
+  "SS0311": {
+    "State": "Jonglei",
+    "County": "Uror",
+    "Population": 122247
+  },
+  "SS0401": {
+    "State": "Lakes",
+    "County": "Awerial",
+    "Population": 84429
+  },
+  "SS0402": {
+    "State": "Lakes",
+    "County": "Cueibet",
+    "Population": 103493
+  },
+  "SS0403": {
+    "State": "Lakes",
+    "County": "Rumbek Centre",
+    "Population": 129894
+  },
+  "SS0404": {
+    "State": "Lakes",
+    "County": "Rumbek East",
+    "Population": 100963
+  },
+  "SS0405": {
+    "State": "Lakes",
+    "County": "Rumbek North",
+    "Population": 45936
+  },
+  "SS0406": {
+    "State": "Lakes",
+    "County": "Wulu",
+    "Population": 50930
+  },
+  "SS0407": {
+    "State": "Lakes",
+    "County": "Yirol East",
+    "Population": 88544
+  },
+  "SS0408": {
+    "State": "Lakes",
+    "County": "Yirol West",
+    "Population": 97770
+  },
+  "SS0501": {
+    "State": "Northern Bahr el Ghazal",
+    "County": "Aweil Centre",
+    "Population": 46561
+  },
+  "SS0502": {
+    "State": "Northern Bahr el Ghazal",
+    "County": "Aweil East",
+    "Population": 235325
+  },
+  "SS0503": {
+    "State": "Northern Bahr el Ghazal",
+    "County": "Aweil North",
+    "Population": 108880
+  },
+  "SS0504": {
+    "State": "Northern Bahr el Ghazal",
+    "County": "Aweil South",
+    "Population": 82487
+  },
+  "SS0505": {
+    "State": "Northern Bahr el Ghazal",
+    "County": "Aweil West",
+    "Population": 134350
+  },
+  "SS0601": {
+    "State": "Unity",
+    "County": "Abiemnhom",
+    "Population": 31847
+  },
+  "SS0602": {
+    "State": "Unity",
+    "County": "Guit",
+    "Population": 43736
+  },
+  "SS0603": {
+    "State": "Unity",
+    "County": "Koch",
+    "Population": 61264
+  },
+  "SS0604": {
+    "State": "Unity",
+    "County": "Leer",
+    "Population": 45519
+  },
+  "SS0605": {
+    "State": "Unity",
+    "County": "Mayendit",
+    "Population": 41092
+  },
+  "SS0606": {
+    "State": "Unity",
+    "County": "Mayom",
+    "Population": 97367
+  },
+  "SS0607": {
+    "State": "Unity",
+    "County": "Panyijiar",
+    "Population": 73559
+  },
+  "SS0608": {
+    "State": "Unity",
+    "County": "Pariang",
+    "Population": 80013
+  },
+  "SS0609": {
+    "State": "Unity",
+    "County": "Rubkona",
+    "Population": 191538
+  },
+  "SS0701": {
+    "State": "Upper Nile",
+    "County": "Baliet",
+    "Population": 46524
+  },
+  "SS0702": {
+    "State": "Upper Nile",
+    "County": "Fashoda",
+    "Population": 45658
+  },
+  "SS0703": {
+    "State": "Upper Nile",
+    "County": "Longochuk",
+    "Population": 44507
+  },
+  "SS0704": {
+    "State": "Upper Nile",
+    "County": "Luakpiny/Nasir",
+    "Population": 203256
+  },
+  "SS0705": {
+    "State": "Upper Nile",
+    "County": "Maban",
+    "Population": 48768
+  },
+  "SS0706": {
+    "State": "Upper Nile",
+    "County": "Maiwut",
+    "Population": 80592
+  },
+  "SS0707": {
+    "State": "Upper Nile",
+    "County": "Malakal",
+    "Population": 47337
+  },
+  "SS0708": {
+    "State": "Upper Nile",
+    "County": "Manyo",
+    "Population": 34825
+  },
+  "SS0709": {
+    "State": "Upper Nile",
+    "County": "Melut",
+    "Population": 82806
+  },
+  "SS0710": {
+    "State": "Upper Nile",
+    "County": "Panyikang",
+    "Population": 16509
+  },
+  "SS0711": {
+    "State": "Upper Nile",
+    "County": "Renk",
+    "Population": 122782
+  },
+  "SS0712": {
+    "State": "Upper Nile",
+    "County": "Ulang",
+    "Population": 85841
+  },
+  "SS0801": {
+    "State": "Warrap",
+    "County": "Gogrial East",
+    "Population": 77558
+  },
+  "SS0802": {
+    "State": "Warrap",
+    "County": "Gogrial West",
+    "Population": 194158
+  },
+  "SS0803": {
+    "State": "Warrap",
+    "County": "Tonj East",
+    "Population": 108542
+  },
+  "SS0804": {
+    "State": "Warrap",
+    "County": "Tonj North",
+    "Population": 139463
+  },
+  "SS0805": {
+    "State": "Warrap",
+    "County": "Tonj South",
+    "Population": 67652
+  },
+  "SS0806": {
+    "State": "Warrap",
+    "County": "Twic",
+    "Population": 164943
+  },
+  "SS0901": {
+    "State": "Western Bahr el Ghazal",
+    "County": "Jur River",
+    "Population": 173602
+  },
+  "SS0902": {
+    "State": "Western Bahr el Ghazal",
+    "County": "Raja",
+    "Population": 43036
+  },
+  "SS0903": {
+    "State": "Western Bahr el Ghazal",
+    "County": "Wau",
+    "Population": 196222
+  },
+  "SS1001": {
+    "State": "Western Equatoria",
+    "County": "Ezo",
+    "Population": 69324
+  },
+  "SS1002": {
+    "State": "Western Equatoria",
+    "County": "Ibba",
+    "Population": 36507
+  },
+  "SS1003": {
+    "State": "Western Equatoria",
+    "County": "Maridi",
+    "Population": 58093
+  },
+  "SS1004": {
+    "State": "Western Equatoria",
+    "County": "Mundri East",
+    "Population": 48615
+  },
+  "SS1005": {
+    "State": "Western Equatoria",
+    "County": "Mundri West",
+    "Population": 29099
+  },
+  "SS1006": {
+    "State": "Western Equatoria",
+    "County": "Mvolo",
+    "Population": 43261
+  },
+  "SS1007": {
+    "State": "Western Equatoria",
+    "County": "Nagero",
+    "Population": 26964
+  },
+  "SS1008": {
+    "State": "Western Equatoria",
+    "County": "Nzara",
+    "Population": 46822
+  },
+  "SS1009": {
+    "State": "Western Equatoria",
+    "County": "Tambura",
+    "Population": 64235
+  },
+  "SS1010": {
+    "State": "Western Equatoria",
+    "County": "Yambio",
+    "Population": 90483
+  }
+};
